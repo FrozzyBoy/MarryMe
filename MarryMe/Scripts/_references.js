2b7c2af1004cac1bddd2dd6bab6ba950ba179320
Artsiom <Artsiom_Marozau@epam.com>
2015-04-05 22:42:00 +0300

    Resolve conflicts
    Signed-off-by: Artsiom <Artsiom_Marozau@epam.com>

--- MarryMe/Scripts/_references.js
+++ MarryMe/Scripts/_references.js
@@ -3,5 +3,4 @@
 /// <reference path="jquery-2.1.3.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
